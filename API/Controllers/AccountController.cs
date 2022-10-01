26d65e58a725a0d83aaaba4239a74117d10bac9b
Hesham_Afifi <hisham.mhammed.afifi@gmail.com>
2022-10-02 00:59:04 +0200

    added dtos and make first request

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+        public AccountController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+
+            if (await UserExists(registerDto.Username)) return BadRequest("UserName is taken");
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
